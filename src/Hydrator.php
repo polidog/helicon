8f1e3fae7781544fa86f602db8aaa443cc5bb0cd
polidog <polidogs@gmail.com>
2019-10-09 03:45:40 +0900

    Change interface.

--- src/Hydrator.php
+++ src/Hydrator.php
@@ -6,7 +6,7 @@ namespace Polidog\Helicon;
 
 interface Hydrator
 {
-    public function hydrate(array $data, string $class): array;
+    public function hydrate(array $data, string $schema): array;
 
     public function extract(array $objectArray): array;
 }
--- src/ObjectHydrator.php
+++ src/ObjectHydrator.php
@@ -21,15 +21,15 @@ class ObjectHydrator implements Hydrator
         $this->propertyAccessor = $propertyAccessor;
     }
 
-    public function hydrate(array $data, string $class): array
+    public function hydrate(array $data, string $schema): array
     {
-        return array_map(function (array $row) use ($class) {
-            $class = new $class();
+        return array_map(function (array $row) use ($schema) {
+            $schema = new $schema();
             foreach (array_keys($row) as $key) {
-                $this->propertyAccessor->setValue($class, $key, $row[$key]);
+                $this->propertyAccessor->setValue($schema, $key, $row[$key]);
             }
 
-            return $class;
+            return $schema;
         }, $data);
     }
 
