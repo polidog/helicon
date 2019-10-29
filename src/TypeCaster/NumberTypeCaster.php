476383e03188ff766bbde767306c33d158307ac8
polidog <polidogs@gmail.com>
2019-10-29 11:19:33 +0900

    Fix NumberTypeCaster

--- src/TypeCaster/NumberTypeCaster.php
+++ src/TypeCaster/NumberTypeCaster.php
@@ -8,6 +8,14 @@ class NumberTypeCaster implements TypeCasterInterface
 {
     public function convert($value, string $type)
     {
+        if (is_int($value) || is_float($value)) {
+            return $value;
+        }
+
+        if (!is_string($value)) {
+            throw new \RuntimeException('Unsupported type ');
+        }
+
         if (preg_match('/^(\d+)$/', $value, $matches)) {
             return (int) $value;
         }
